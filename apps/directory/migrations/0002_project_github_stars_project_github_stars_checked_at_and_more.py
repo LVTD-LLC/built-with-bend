a039d4fb467ea79245fe21887c33f2d3a2ab5993
Forge <forge@gregagi.com>
2026-09-24 06:58:44 +0000

    Defer slug lookup index until after the backfill

--- apps/directory/migrations/0002_project_github_stars_project_github_stars_checked_at_and_more.py
+++ apps/directory/migrations/0002_project_github_stars_project_github_stars_checked_at_and_more.py
@@ -46,7 +46,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='project',
             name='slug',
-            field=models.SlugField(blank=True, max_length=160, null=True),
+            field=models.SlugField(blank=True, db_index=False, max_length=160, null=True),
         ),
         migrations.AddField(
             model_name='project',
